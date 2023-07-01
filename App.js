414ac0f02bb7eb1691f97867086b4fbc5fb37cec
richidoes <richard.deleon.garcia@gmail.com>
2023-06-30 21:32:56 -0600

    chore: :heavy_plus_sign: Add tailwindCSS for styling

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
+import { useColorScheme } from 'nativewind';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 
 export default function App() {
+  const { toggleColorScheme } = useColorScheme();
+
   return (
-    <View style={styles.container}>
+    <View className="flex-1 items-center justify-center bg-white dark:bg-slate-800">
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Dark mode" onPress={toggleColorScheme} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
